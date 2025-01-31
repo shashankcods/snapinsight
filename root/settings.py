749f554ddd2065cf963d34d1577b0d76ed6a2d72
kyrolxg <harishraju658@gmail.com>
2025-02-01 00:25:42 +0530

    Fixed CSS file linking and updated settings.py

--- root/settings.py
+++ root/settings.py
@@ -85,3 +85,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'photoapp/static')
+]
